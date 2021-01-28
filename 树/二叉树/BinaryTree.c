ea9eada3e10f2334b0e3edd0ac2303a571570b55
yanshengran <77721305+yanshengran@users.noreply.github.com>
2021-01-28 19:11:51 +0800

    Update BinaryTree.c

--- 树/二叉树/BinaryTree.c
+++ 树/二叉树/BinaryTree.c
@@ -2,7 +2,7 @@
 //  BinaryTree.c
 //  BinaryTree
 //
-//  Created by 颜圣燃 on 2021/1/27.
+//  Created by yanshengran on 2021/1/27.
 //
 
 #include "BinaryTree.h"
@@ -13,14 +13,14 @@ struct TreeNode{
     Node left;
     Node right;
 };
-BinaryTree InitBiTree(ElementType X){
+BinaryTree InitBiTree(ElementType X){//初始化一个二叉树
     BinaryTree BT = (BinaryTree)malloc(sizeof(struct TreeNode));
     BT->element = X;
     BT->left = NULL;
     BT->right = NULL;
     return BT;
 }
-BinaryTree MakeEmpty(BinaryTree T){
+BinaryTree MakeEmpty(BinaryTree T){//清空一个二叉树
     if(T != NULL){
         MakeEmpty(T->left);
         MakeEmpty(T->right);
@@ -28,25 +28,25 @@ BinaryTree MakeEmpty(BinaryTree T){
     }
     return NULL;
 }
-Node Find(ElementType X, BinaryTree T){
+Node Find(ElementType X, BinaryTree T){//根据值返回二叉树的节点
     if(T == NULL)return NULL;
     if(X < T->element)return Find(X, T->left);
     if(X > T->element)return Find(X, T->right);
     else return T;
 }
-Node FindMin(BinaryTree T){
+Node FindMin(BinaryTree T){//找到二叉树的最小节点
     if(T == NULL)return NULL;
     while(T->left != NULL){
         T = T->left;
     }return T;
 }
-Node FindMax(BinaryTree T){
+Node FindMax(BinaryTree T){//找到二叉树的最大节点
     if(T == NULL)return NULL;
     while(T->right != NULL){
         T = T->right;
     }return T;
 }
-BinaryTree Insert(ElementType X, BinaryTree T){
+BinaryTree Insert(ElementType X, BinaryTree T){//向一棵二叉树中插入元素
     if(T == NULL){
         T = (BinaryTree)malloc(sizeof(struct TreeNode));
         if(T == NULL){printf("动态内存申请失败！");}
@@ -58,7 +58,7 @@ BinaryTree Insert(ElementType X, BinaryTree T){
     else if(X < T->element) T->left = Insert(X, T->left);
     return T;
 }
-BinaryTree Delete(ElementType X, BinaryTree T){
+BinaryTree Delete(ElementType X, BinaryTree T){//删除一个节点
     Node temp;
     if(T == NULL){printf("未找到元素\n");}
     else if(X < T->element)T->left = Delete(X, T->left);
@@ -82,21 +82,52 @@ BinaryTree Delete(ElementType X, BinaryTree T){
     }
     return T;
 }
-void PreOrderTraversal(BinaryTree T){
+void PreOrderTraversal(BinaryTree T){//前序遍历
     if(T == NULL)return;
     printf("%d ", T->element);
     PreOrderTraversal(T->left);
     PreOrderTraversal(T->right);
 }
-void PostOrderTraversal(BinaryTree T){
+void PostOrderTraversal(BinaryTree T){//后序遍历
     if(T == NULL)return;
     PostOrderTraversal(T->left);
     PostOrderTraversal(T->right);
     printf("%d ", T->element);
 }
-void InOrderTraversal(BinaryTree T){
+void InOrderTraversal(BinaryTree T){//中序遍历
     if(T == NULL)return;
     InOrderTraversal(T->left);
     printf("%d ", T->element);
     InOrderTraversal(T->right);
+}
+void InOrderTraversalStack(BinaryTree T){//中序遍历的非递归实现
+    Node Stack[100];
+    int top = -1;
+    BinaryTree P = T;
+    while(P || top != -1){
+        if(P){top++; Stack[top] = P; P = P->left;}
+        else{
+            printf("%d ", Stack[top]->element);
+            P = Stack[top]->right;
+            top--;
+        }
+    }
+}
+void LevelTraversal(BinaryTree T){//二叉树的层次遍历
+    Node Queue[100];
+    int front = 0, rear = 0;
+    Queue[rear] = T;
+    rear = (rear+1)%100;
+    while(!(rear == front)){
+        printf("%d ",Queue[front]->element);
+        if(Queue[front]->left != NULL){
+            Queue[rear] = Queue[front]->left;
+            rear = (rear+1)%100;
+        }
+        if(Queue[front]->right != NULL){
+            Queue[rear] = Queue[front]->right;
+            rear = (rear+1)%100;
+        }
+        front = (front+1)%100;
+    }
 }
